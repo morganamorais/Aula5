30f9e14761748884f27c6ef65d9619eedcbed5b9
Morgana Morais <morganamorais25@gmail.com>
2022-03-21 15:21:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Aula5
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int mes;
+
+            System.Console.WriteLine("Digite um número do mês: ");
+           mes = int.Parse(Console.ReadLine());
+
+            switch(mes)
+            {     
+                case 1:                
+                System.Console.WriteLine("Janeiro");
+                break;
+                case 2:
+                System.Console.WriteLine("Fevereiro");
+                break;
+                case 3:
+                System.Console.WriteLine("Março");
+                break;
+                case 4:
+                System.Console.WriteLine("Abril");
+                break;
+                case 5:
+                System.Console.WriteLine("Maio");
+                break;
+                case 6:
+                System.Console.WriteLine("Junho");
+                break;
+                case 7:
+                System.Console.WriteLine("Julho");
+                break;
+                case 8:
+                System.Console.WriteLine("Agosto");
+                break;
+                case 9:
+                System.Console.WriteLine("Setembro");
+                break;
+                case 10:
+                System.Console.WriteLine("Outrubro");
+                break;
+                case 11:
+                System.Console.WriteLine("Novembro");
+                break;
+                case 12:
+                System.Console.WriteLine("Dezembro");
+                break;
+                default :
+                System.Console.WriteLine("Mês inválido!");
+                break;
+               
+                
+                }
+            }
+
+            
+        }
+    }
